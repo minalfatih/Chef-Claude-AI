3f502afa897db6cae4bb1e974ca7e407f4ddc952
Amina <minalfatih220@gmail.com>
2026-02-17 12:12:19 +0200

    Refactor font family in App.css and correct typo in Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p style={{ marginBottom: '5px' }}><span>&copy;</span> 2026 Chef Claude. All rights reserved.</p>
-      <span>Codeed by</span> {' '}
+      <span>Coded by</span> {' '}
       <a
         href="https://linkedin.com/in/amina-elfatih-4aa4b0192"
         target="_blank"
